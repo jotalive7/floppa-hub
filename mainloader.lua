c24f44758209a63564d9a5e918a757ed5decc66d
JoTa <145311605+jota-js@users.noreply.github.com>
2024-08-19 00:26:33 -0300

    Create mainloader.lua

---
+++ mainloader.lua
@@ -0,0 +1,18 @@
+--[[
+Floppa Hub v1.1
+(Beta Acess)
+
+Mirrors:
+
+1) Main Ui
+2) Auto Bounty
+3) Kaitun { NÃO FIZ AINDA }
+]]
+
+if version == 1 then
+
+else if version == 2 then
+
+else if version == 3 then
+
+    end
