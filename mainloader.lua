ee7d8c168ba5b3eab4040c7e955cd1bb733379f5
JoTa <145311605+jota-js@users.noreply.github.com>
2024-08-25 20:30:55 -0300

    Delete mainloader.lua

--- mainloader.lua
+++
@@ -1,7 +0,0 @@
-if version == 1 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/jota-js/FloppaHub/main/gui.lua"))()
-else if version == 2 then
-  print('Not released')
-else if version == 3 then
-  print('Not released')
-end
