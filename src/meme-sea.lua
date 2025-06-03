474d7bc1d295b7f1a43d01db3f1441eefe56fbf7
JoTa <145311605+jotalive7@users.noreply.github.com>
2025-06-03 03:18:23 -0300

    Update meme-sea.lua

--- src/meme-sea.lua
+++ src/meme-sea.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Floppa Hub still at development, join https://discord.gg/5cNCWgPQaN")
+game.Players.LocalPlayer:Kick("Floppa Hub for meme sea still at development, join https://discord.gg/5cNCWgPQaN")
