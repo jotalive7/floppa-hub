46217614395439c25697ccf7e48625614ba66a83
JoTa <145311605+jotalive7@users.noreply.github.com>
2025-06-03 03:44:24 -0300

    Update main-loader.lua

--- main-loader.lua
+++ main-loader.lua
@@ -3,7 +3,7 @@ local currentPlaceId = game.PlaceId
 if currentPlaceId == 10260193230 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/jotalive7/floppa-hub/refs/heads/main/src/meme-sea.lua"))()
 elseif currentPlaceId == 7449423635 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/jotalive7/floppa-hub/refs/heads/main/src/blox-fruits"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jotalive7/floppa-hub/refs/heads/main/src/blox-fruits.lua"))()
 else
   game.Players.LocalPlayer:Kick("Game not supported, only blox and meme sea.")
 end
